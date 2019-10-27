b29ff6ae0e4c255f69134880d190c7d644cc67cb
dinarashok <dinara.shokabay@gmail.com>
2019-10-27 19:14:15 +0600

    Update main.go

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ import (
 func main() {
   args := os.Args[1:]
   //Create empty board
-  var board [9][9]rune
+  var SolutionTable [9][9]rune
   //Fill the board with arg values
   for i:=range args {
     temp_rune := []rune(args[i])
     for j:=range temp_rune {
-      board[i][j] = temp_rune[j]
+      SolutionTable[i][j] = temp_rune[j]
     }
   }
 
   //********ADD CONDITION FOR VALIDITY OF GIVEN NUMBERS*************
-  if checkBoard(board) {
+  if checkBoard(SolutionTable) {
     //Solve sudoku
-    solve(&board, 0, 0)
+    solve(&SolutionTable, 0, 0)
 
     for i:=0; i<9; i++ {
       for j:=0; j<9; j++ {
-        z01.PrintRune(board[i][j])
+        z01.PrintRune(SolutionTable[i][j])
       }
       z01.PrintRune(10)
     }
@@ -35,8 +35,8 @@ func main() {
 
 }
 
-func solve(board *[9][9]rune, row, col int) bool {
-  temp_board := *board
+func solve(SolutionTable *[9][9]rune, row, col int) bool {
+  temp_board := *SolutionTable
   //Last column, Go to next row
   if col == 9 {
     row++
@@ -49,33 +49,33 @@ func solve(board *[9][9]rune, row, col int) bool {
 
   //If cell is already filled, skip it, go to next value in row
   if temp_board[row][col] != '.' {
-    *board = temp_board
-    return solve(board, row, col+1)
+    *SolutionTable = temp_board
+    return solve(SolutionTable, row, col+1)
   }
 
   for i:=1; i<=9;i++ {
     r := getRune(i)
 
-    *board = temp_board
-    if isValid(board, row, col, r) {
+    *SolutionTable = temp_board
+    if isValid(SolutionTable, row, col, r) {
       //Place this rune to board after checking it validity
       temp_board[row][col] = r
       //Check if it works for next values
-      *board = temp_board
-      if solve(board, row, col + 1) {
+      *SolutionTable = temp_board
+      if solve(SolutionTable, row, col + 1) {
         return true
       }
       //Condition when it does not work, Return empty entry
       temp_board[row][col] = '.'
-      *board = temp_board
+      *SolutionTable = temp_board
     }
   }
   return false
 }
 
 //Check for validity of given rune accodring to row, col and subgrid
-func isValid(board *[9][9]rune, row, col int, r rune) bool {
-  temp_board := *board
+func isValid(SolutionTable *[9][9]rune, row, col int, r rune) bool {
+  temp_board := *SolutionTable
   //Check column
   for i:=0; i<9; i++ {
     if temp_board[row][i] == r {
@@ -118,13 +118,13 @@ func getRune(i int) rune {
 }
 
 //Check board before solving
-func checkBoard(board [9][9]rune) bool {
+func checkBoard(SolutionTable [9][9]rune) bool {
   for i:=0; i<9; i++ {
     for j:=0; j<9; j++ {
-      if board[i][j] == 46 {
+      if SolutionTable[i][j] == 46 {
         continue
       }
-      if !boardValid(board, i, j) {
+      if !boardValid(SolutionTable, i, j) {
         return false
       }
     }
@@ -132,18 +132,18 @@ func checkBoard(board [9][9]rune) bool {
   return true
 }
 
-func boardValid(board [9][9]rune, i, j int) bool {
+func boardValid(SolutionTable [9][9]rune, i, j int) bool {
   for m := 0; m < 9; m++ {
-		if m != j && board[i][m] == board[i][j] {
+		if m != j && SolutionTable[i][m] == SolutionTable[i][j] {
       return false
     }
-		if (m != i && board[m][j] == board[i][j]) {
+		if (m != i && SolutionTable[m][j] == SolutionTable[i][j]) {
       return false
     }
 	}
 	for m := i / 3 * 3; m < i / 3 * 3 + 3; m++ {
 		for n := j / 3 * 3; n < j / 3 * 3 + 3; n++ {
-			if m != i && n != j && board[m][n] == board[i][j] {
+			if m != i && n != j && SolutionTable[m][n] == SolutionTable[i][j] {
         return false
       }
 		}
